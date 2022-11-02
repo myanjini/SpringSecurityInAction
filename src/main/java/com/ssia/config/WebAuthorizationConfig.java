6de52e5929554b0343880e3043e6ee50ba9f4402
naanjini@gmail.com <naanjini@gmail.com>
2022-11-02 17:34:49 +0900

    오류 수정
    - 설정 클래스의 구현이 바뀐 것을 수정

--- src/main/java/com/ssia/config/WebAuthorizationConfig.java
+++ src/main/java/com/ssia/config/WebAuthorizationConfig.java
@@ -1,29 +1,15 @@
 package com.ssia.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class WebAuthorizationConfig {
+public class WebAuthorizationConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	public UserDetailsService userDetailsService() {
-		var userDetailsService = new InMemoryUserDetailsManager();
-		var user = User.withUsername("john")
-					.password("12345")
-					.authorities("read")
-					.build();
-		userDetailsService.createUser(user);
-		return userDetailsService;
-	}
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
+		http.authorizeRequests().anyRequest().authenticated();
 	}
 }
