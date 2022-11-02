438d2b3c469a958b23a9db61b933c2b245a091c0
naanjini@gmail.com <naanjini@gmail.com>
2022-11-02 14:17:19 +0900

    AuthenticationProvider 구현 재정의
    - AuthenticationProvider를 사용하여 사용자 정의 인증 논리를 구현

--- src/main/java/com/ssia/controller/HelloController.java
+++ src/main/java/com/ssia/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.ssia.controller;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	UserDetailsService uds;
+	
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello!";
