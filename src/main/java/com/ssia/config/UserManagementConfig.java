7cc87495cab74043ad1db54effe0f203a6ee8f3a
naanjini@gmail.com <naanjini@gmail.com>
2022-11-02 17:58:20 +0900

    UserDetailsService 계약 구현
    - Spring Security가 필요로 하는 username으로 사용자를 검색하는 기능을 구현

--- src/main/java/com/ssia/config/UserManagementConfig.java
+++ src/main/java/com/ssia/config/UserManagementConfig.java
@@ -1,25 +1,25 @@
 package com.ssia.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import com.ssia.model.User;
+import com.ssia.services.InMemoryUserDetailsService;
 
 @Configuration
 public class UserManagementConfig {
 
 	@Bean
 	public UserDetailsService userDetailsService() {
-		var userDetailsService = new InMemoryUserDetailsManager();
-		var user = User.withUsername("john")
-					.password("12345")
-					.authorities("read")
-					.build();
-		userDetailsService.createUser(user);
-		return userDetailsService;
+		UserDetails u = new User("john", "12345", "read");
+		List<UserDetails> users = List.of(u);
+		return new InMemoryUserDetailsService(users);
 	}
 	
 	@Bean
