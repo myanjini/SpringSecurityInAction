3e2707db3e003f4d43332cc225e45c508425ba19
naanjini@gmail.com <naanjini@gmail.com>
2022-11-03 15:46:51 +0900

    DelegatingPasswordEncoder를 사용한 다중 인코딩 전략
    - 암호의 접두사를 기반으로 올바른 PasswordEncoder 구현을 위임

--- src/main/java/com/ssia/config/ProjectConfig.java
+++ src/main/java/com/ssia/config/ProjectConfig.java
@@ -1,10 +1,16 @@
 package com.ssia.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.ldap.DefaultLdapUsernameToDnMapper;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
@@ -25,6 +31,10 @@ public class ProjectConfig {
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		Map<String, PasswordEncoder> encoders = new HashMap<>();
+		encoders.put("noop", NoOpPasswordEncoder.getInstance());
+		encoders.put("bcrypt", new BCryptPasswordEncoder());
+		encoders.put("scrypt", new SCryptPasswordEncoder());
+		return new DelegatingPasswordEncoder("bcrypt", encoders);
 	}
 }
