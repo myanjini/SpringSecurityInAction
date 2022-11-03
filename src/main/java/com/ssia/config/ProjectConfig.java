c5c2966d0bdd56e28d9cd5763ee92262e87bfb76
naanjini@gmail.com <naanjini@gmail.com>
2022-11-03 18:03:51 +0900

    커스텀 인증 로직 구현
    - AuthenticationProvider 인터페이스를 상속한 구현 클래스 추가
    - supports 및 authenticate 메서드를 재정의
    - 프로젝트의 구성 클래스에서 WebSecurityConfigurerAdapter 클래스의 configure 메서드를 재정의

--- src/main/java/com/ssia/config/ProjectConfig.java
+++ src/main/java/com/ssia/config/ProjectConfig.java
@@ -3,8 +3,12 @@ package com.ssia.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
@@ -16,8 +20,16 @@ import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private AuthenticationProvider authenticationProvider;
+	
+	@Override 
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider);
+	}
+	
 	@Bean
 	public UserDetailsService userDetailsService() {
 		var cs = new DefaultSpringSecurityContextSource("ldap://127.0.0.1:33389/dc=springframework,dc=org");
