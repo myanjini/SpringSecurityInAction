040886db4d55abbfee831308ba8d4e8bf4e2702e
naanjini@gmail.com <naanjini@gmail.com>
2022-11-04 12:50:13 +0900

    HTTP Basic 인증 적용

--- src/main/java/com/ssia/config/ProjectConfig.java
+++ src/main/java/com/ssia/config/ProjectConfig.java
@@ -1,51 +1,14 @@
 package com.ssia.config;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.ldap.DefaultLdapUsernameToDnMapper;
-import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
-import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
 
 @Configuration
-@EnableAsync
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private AuthenticationProvider authenticationProvider;
-	
-	@Override 
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(authenticationProvider);
-	}
-	
-	@Bean
-	public UserDetailsService userDetailsService() {
-		var cs = new DefaultSpringSecurityContextSource("ldap://127.0.0.1:33389/dc=springframework,dc=org");
-		cs.afterPropertiesSet();
-		
-		var userDetailsManager = new LdapUserDetailsManager(cs);
-		userDetailsManager.setUsernameMapper(new DefaultLdapUsernameToDnMapper("ou=groups", "uid"));
-		userDetailsManager.setGroupSearchBase("ou=groups");
-		return userDetailsManager;
-	}
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
-	
-	@Bean
-	public InitializingBean initializingBean() {
-		return () -> SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_GLOBAL);
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
 	}
 }
