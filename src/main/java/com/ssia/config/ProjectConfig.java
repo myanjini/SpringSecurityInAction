3900ff1dde314981bf5a8a1232c35bd07027fc1c
naanjini@gmail.com <naanjini@gmail.com>
2022-11-04 13:15:51 +0900

    AuthenticationEntryPoint 구현
    - 실패한 인증에 대한 사용자 지정 응답(상태 코드와 메시지)을 설정
    - authenticationEntryPoint 메서드를 통해 설정

--- src/main/java/com/ssia/config/ProjectConfig.java
+++ src/main/java/com/ssia/config/ProjectConfig.java
@@ -1,14 +1,34 @@
 package com.ssia.config;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic();
+		http.httpBasic(c -> {
+			c.realmName("OTHER");
+			c.authenticationEntryPoint(new AuthenticationEntryPoint() {
+				@Override
+				public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+						throws IOException, ServletException {
+					response.addHeader("message", "Luke, I'm your father!");
+					response.sendError(HttpStatus.UNAUTHORIZED.value());
+				}
+			});
+		});
+		http.authorizeRequests().anyRequest().authenticated();
 	}
 }
