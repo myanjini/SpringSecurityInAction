8ccd1b5817aef9b4674d4214c941355d0bfa9505
naanjini@gmail.com <naanjini@gmail.com>
2022-11-04 14:29:17 +0900

    form-based 로그인을 통한 인증 구현
    - 구성 클래스의 configure(HttpSecurity http) 메서드에서 HttpSecurity 파라미터의
    formLogin() 메서드를 호출
    - @Controller로 변경
    - 뷰 페이지 추가

--- src/main/java/com/ssia/controller/HelloController.java
+++ src/main/java/com/ssia/controller/HelloController.java
@@ -1,12 +1,12 @@
 package com.ssia.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
-	@GetMapping("/hello") 
-	public String hello() {
-		return "Hello";
+	@GetMapping("/home") 
+	public String home() {
+		return "home.html";
 	}
 }
