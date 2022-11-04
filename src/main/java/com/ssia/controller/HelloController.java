9c2ce53925ad6d9b889b06dd6f6af79fcec282e7
naanjini@gmail.com <naanjini@gmail.com>
2022-11-04 11:40:46 +0900

    DelegatingSecurityContextRunnable을 사용하여 보안 컨텍스트 전달

--- src/main/java/com/ssia/controller/HelloController.java
+++ src/main/java/com/ssia/controller/HelloController.java
@@ -1,6 +1,11 @@
 package com.ssia.controller;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,5 +26,21 @@ public class HelloController {
 		String username = context.getAuthentication().getName();
 		
 		// do something with the username
+	}
+	
+	@GetMapping("/ciao")
+	public String ciao() throws Exception {
+		Callable<String> task = () -> {
+			SecurityContext context = SecurityContextHolder.getContext();
+			return context.getAuthentication().getName();
+		};
+		
+		ExecutorService e = Executors.newCachedThreadPool();
+		try {
+			var contextTask = new DelegatingSecurityContextCallable<>(task);
+			return "Ciao, " + e.submit(contextTask).get() + "!";
+		} finally {
+			e.shutdown();
+		}
 	}
 }
