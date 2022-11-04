71a0c2905f76f44afed502110e5be0d3595ac893
naanjini@gmail.com <naanjini@gmail.com>
2022-11-04 18:03:30 +0900

    프로젝트 초기화
    - 테이블 및 데이터 생성 쿼리 작성
    - DataSource 관련 설정
    - 불필요한 파일 삭제

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,17 @@
-CREATE TABLE IF NOT EXISTS `spring`.`users` (   
-`id` INT NOT NULL AUTO_INCREMENT,
-`username` VARCHAR(45) NOT NULL,   
-`password` VARCHAR(45) NOT NULL,   
-`enabled` INT NOT NULL,
-PRIMARY KEY (`id`));
+CREATE TABLE IF NOT EXISTS `spring`.`user` (   
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL, 
+  `password` TEXT NOT NULL,
+  `algorithm` VARCHAR(45) NOT NULL, 
+  PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS `spring`.`authorities` (   
-	`id` INT NOT NULL AUTO_INCREMENT,
-  	`username` VARCHAR(45) NOT NULL,   
-  	`authority` VARCHAR(45) NOT NULL,   
-  	PRIMARY KEY (`id`));
+ CREATE TABLE IF NOT EXISTS `spring`.`authority` (   `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,   `user` INT NOT NULL,
+  PRIMARY KEY (`id`));
+  
+ CREATE TABLE IF NOT EXISTS `spring`.`product` ( 
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL, 
+  `price` VARCHAR(45) NOT NULL, 
+  `currency` VARCHAR(45) NOT NULL, 
+  PRIMARY KEY (`id`));
