d5d5b3fe2088f97ee17e3b83fe01bca23ab1aeb3
naanjini@gmail.com <naanjini@gmail.com>
2022-11-04 09:19:03 +0900

    SecurityContextHolder의 MODE_THREADLOCAL 전략 샘플
    - 인증한 사용자 정보를 가져와서 출력

---
+++ src/main/java/com/ssia/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.ssia.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/hello") 
+	public String hello() {
+		SecurityContext context = SecurityContextHolder.getContext();
+		Authentication auth = context.getAuthentication();
+		
+		return "Hello, " + auth.getName() + "!";
+	}
+}
