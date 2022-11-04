606c4fac4fd107d0111cea852dbcc1381131d3b3
naanjini@gmail.com <naanjini@gmail.com>
2022-11-04 16:47:59 +0900

    커스텀 인증 로직 구현
    - AuthenticationSuccessHandler 인증 성공 시 실행 로직 구현
    - AuthenticationFailureHandler 인증 실패 시 실행 로직 구현
    - 설정 클래스의 formLogin() 메서드에서 반환된 FormLoginConfigurer 객체에 핸들러 추가

--- src/main/java/com/ssia/config/ProjectConfig.java
+++ src/main/java/com/ssia/config/ProjectConfig.java
@@ -1,15 +1,27 @@
 package com.ssia.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import com.ssia.handlers.CustomAuthenticationSuccessHandler;
+import com.ssia.handlers.CustomerAuthenticationFailureHandler;
+
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+	
+	@Autowired
+	CustomerAuthenticationFailureHandler customerAuthenticationFailureHandler;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin();
+		http.formLogin()
+			.successHandler(customAuthenticationSuccessHandler)
+			.failureHandler(customerAuthenticationFailureHandler);
 		http.authorizeRequests().anyRequest().authenticated();
 	}
 }
