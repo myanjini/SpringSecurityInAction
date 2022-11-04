161cddcb6da20ecc0840ad47f8a8cacae142a07e
naanjini@gmail.com <naanjini@gmail.com>
2022-11-04 10:47:54 +0900

    SecurityContextHolder의 MODE_INHERITABLETHREADLOCAL 전략 샘플
    - 보안 컨텍스트가 비동기 메서드의 새로 생성된 스레드로 전파되는 것을 확인
    - SecurityContextHolder.setStrategyName() 메소드를 호출해 전략을 설정
    - @Async 및 @EnableAsync 어노테이션을 이용해 엔드포인트 실행을 비동기로 처리

--- src/main/java/com/ssia/config/ProjectConfig.java
+++ src/main/java/com/ssia/config/ProjectConfig.java
@@ -1,25 +1,23 @@
 package com.ssia.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.ldap.DefaultLdapUsernameToDnMapper;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
 
 @Configuration
+@EnableAsync
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -44,5 +42,10 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Bean
+	public InitializingBean initializingBean() {
+		return () -> SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
 	}
 }
--- src/main/java/com/ssia/controller/HelloController.java
+++ src/main/java/com/ssia/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.ssia.controller;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,14 @@ public class HelloController {
 	@GetMapping("/hello") 
 	public String hello(Authentication auth) {
 		return "Hello, " + auth.getName() + "!";
+	}
+	
+	@GetMapping("/bye")
+	@Async
+	public void goodbye() {
+		SecurityContext context = SecurityContextHolder.getContext();
+		String username = context.getAuthentication().getName();
+		
+		// do something with the username
 	}
 }
