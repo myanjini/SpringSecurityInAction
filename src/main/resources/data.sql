01cca1d833f5ea789561ea3a77822ab6f482fd0d
naanjini@gmail.com <naanjini@gmail.com>
2022-11-07 15:24:01 +0900

    오류 수정
    - 테스트 데이터 수정 (패스워드 값 변경)
    - Spring Boot 버전 변경에 따른 설정 정보 변경
    - 의존 정보 정리

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,4 @@
-INSERT IGNORE INTO `spring`.`user` (`id`, `username`, `password`, `algorithm`) VALUES ('1', 'john', '$2a$10$xn3LI/
-AjqicFYZFruSwve.681477XaVNaUQbr1gioaWPn4t1KsnmG', 'BCRYPT');
+INSERT IGNORE INTO `spring`.`user` (`id`, `username`, `password`, `algorithm`) VALUES ('1', 'john', '$2a$10$K8BkZU9KVP1qB0NBI/m.meehPwwpHZ2suKYj0uYjscpOh7Yfgak2.', 'BCRYPT');
 INSERT IGNORE INTO `spring`.`authority` (`id`, `name`, `user`) VALUES ('1', 'READ', '1');
 INSERT IGNORE INTO `spring`.`authority` (`id`, `name`, `user`) VALUES ('2', 'WRITE', '1');
 INSERT IGNORE INTO `spring`.`product` (`id`, `name`, `price`, `currency`) VALUES ('1', 'Chocolate', '10', 'USD');
