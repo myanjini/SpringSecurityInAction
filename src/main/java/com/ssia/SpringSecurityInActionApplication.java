6d421f1e5b755ce67cdf56367c074254338710b8
naanjini@gmail.com <naanjini@gmail.com>
2022-11-09 18:09:07 +0900

    프로젝트 초기화

--- src/main/java/com/ssia/SpringSecurityInActionApplication.java
+++ src/main/java/com/ssia/SpringSecurityInActionApplication.java
@@ -2,9 +2,8 @@ package com.ssia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
+@SpringBootApplication
 public class SpringSecurityInActionApplication {
 
 	public static void main(String[] args) {
