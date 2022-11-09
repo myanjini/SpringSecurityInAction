3cb1cc75db83cc503873a69b30e1f1a87ab51b91
naanjini@gmail.com <naanjini@gmail.com>
2022-11-09 15:13:16 +0900

    체인의 다른 위치에 필터 추가
    - Authorization 요청 헤더의 값을 검증하는 필터를 추가
    - 기본 UserDetailsService의 구성을 비활성화

--- src/main/java/com/ssia/config/ProjectConfig.java
+++ src/main/java/com/ssia/config/ProjectConfig.java
@@ -1,19 +1,22 @@
 package com.ssia.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-import com.ssia.filters.AuthenticationLoggingFilter;
-import com.ssia.filters.RequestValidationFilter;
+import com.ssia.filters.StaticKeyAuthenticationFilter;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private StaticKeyAuthenticationFilter filter;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.addFilterBefore(new RequestValidationFilter(),	BasicAuthenticationFilter.class)
-			.addFilterAfter(new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class)
+		http.addFilterAt(filter, BasicAuthenticationFilter.class)
 			.authorizeRequests()
 				.anyRequest().permitAll();
 	} 
